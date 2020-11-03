5c0d7439ec9cd94d7db7e9149a619f58de468e0d
Eero Sova <t7soee00@students.oamk.fi>
2020-11-03 10:08:51 +0200

    Added connection to DB and users table

---
+++ backend/knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+require('dotenv').config()
+
+
+module.exports = {
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+  migrations: {
+    directory: './db/migrations',
+  },
+  seeds: {
+    directory: './db/seeds',
+  },
+
+};
