ef08bb2b4d8bdb3370f4943e5b2eee25c3006df4
Long <longlong2000hcm@gmail.com>
2020-11-04 12:32:43 +0200

    install react-router-dom, setup basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Welcome from "./components/welcome";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      example_state: "example state string"
+    }
+  }
+  render() {
+    return (
+      <BrowserRouter>
+      <Switch>
+      <Route
+            path="/"
+            render={routerProps => (
+              <Welcome {...routerProps} example_state={this.state.example_state}/>
+            )}
+          />
+      </Switch>
+    </BrowserRouter>
+    )
+  }
 }
-
-export default App;
