fda6aa79bda123d430b6cd51b39fc26e7ab969c0
Long <longlong2000hcm@gmail.com>
2020-12-03 20:10:02 +0200

    some css for reservation page

--- frontend/src/components/Reservation.js
+++ frontend/src/components/Reservation.js
@@ -8,15 +8,31 @@ const RenderButton = (props) => {
     }
     let result = index.map(i => {
         if (props.busy.includes(i))
-            return <button className={`col ${styles.busyBtn}`} key={i}>{`${i}:00-${i + 1}:00`}</button>
+            return <button className={`col ${styles.busyBtn}`} key={i}>
+                {`${i}:00-${i + 1}:00`}
+                <div className="row"><div className="col"></div></div>
+                <div className="row"><div className={`col ${styles.busyTxt}`}>Busy</div></div>
+            </button>
         else if (props.owned.includes(i))
-            return <button className={`col ${styles.ownedBtn}`} key={i}>{`${i}:00-${i + 1}:00`}</button>
+            return <button className={`col ${styles.ownedBtn}`} key={i}>
+                {`${i}:00-${i + 1}:00`}
+                <div className="row"><div className="col"></div></div>
+                <div className="row"><div className={`col ${styles.ownedTxt}`}>Owned</div></div>
+            </button>
         else if (props.reserve.includes(i))
             return <button className={`col ${styles.reservedBtn}`} key={i}
-                onClick={() => props.handleTimeSelect(i)}>{`${i}:00-${i + 1}:00`}</button>
+                onClick={() => props.handleTimeSelect(i)}>
+                {`${i}:00-${i + 1}:00`}
+                <div className="row"><div className="col"></div></div>
+                <div className="row"><div className={`col ${styles.reservedTxt}`}>Selected</div></div>
+            </button>
         else
             return <button className={`col ${styles.freeBtn}`} key={i}
-                onClick={() => props.handleTimeSelect(i)}>{`${i}:00-${i + 1}:00`}</button>
+                onClick={() => props.handleTimeSelect(i)}>
+                {`${i}:00-${i + 1}:00`}
+                <div className="row"><div className="col"></div></div>
+                <div className="row"><div className="col text-secondary">Free</div></div>
+            </button>
     }
     )
     return <div className="row">{result}</div>
@@ -34,7 +50,7 @@ export default class Reservation extends Component {
         }
         this.selectTime = this.selectTime.bind(this);
         this.selectDate = this.selectDate.bind(this);
-        this.handleTimeSelect= this.handleTimeSelect.bind(this)
+        this.handleTimeSelect = this.handleTimeSelect.bind(this)
     }
     selectTime(data) {
         this.setState({ selectTime: data })
@@ -47,8 +63,8 @@ export default class Reservation extends Component {
     handleTimeSelect(i) {
         if (this.state.reserve.includes(i)) {
             let tempArray = this.state.reserve;
-            tempArray.splice(this.state.reserve.indexOf(i),1)
-            this.setState({reserve: tempArray})
+            tempArray.splice(this.state.reserve.indexOf(i), 1)
+            this.setState({ reserve: tempArray })
         }
         else
             this.setState({ reserve: [...this.state.reserve, i] })
@@ -60,25 +76,28 @@ export default class Reservation extends Component {
         let pass = (({ busy, owned, reserve }) => ({ busy, owned, reserve }))(this.state);
         return (
             <div className="container">
-                {/* <div className="row">
-                    <div style={{ backgroundColor: "white" }}>Selecte time: {this.state.selectedTime}</div>
-                </div>
+
                 <div className="row">
-                    <Example selectTime={this.selectTime}></Example>
-                </div> */}
-                <div className="row">
+                    <div className="p-3 bg-white">Date:</div>
                     <input type="date" className=""
                         min={new Date().toISOString().substr(0, 10)}
                         defaultValue={this.state.date}
                         onChange={e => this.selectDate(e)}
                     />
                 </div>
+                <RenderButton min={0} max={6} {...pass} handleTimeSelect={this.handleTimeSelect} />
+                <RenderButton min={6} max={12} {...pass} handleTimeSelect={this.handleTimeSelect} />
+                <RenderButton min={12} max={18} {...pass} handleTimeSelect={this.handleTimeSelect} />
+                <RenderButton min={18} max={24} {...pass} handleTimeSelect={this.handleTimeSelect} />
+                <div className="row">Selected time</div>
+                <ul>
+                {[...this.state.reserve].sort((a,b)=>a-b).map((x)=>{
+                    return <li className="row">{`${x}:00-${x + 1}:00`}</li>
+                })}
+                </ul>
                 <div className="row">
-                    <button className="col" onClick={() => this.handleSubmit()}>Submit</button>
+                    <button className="btn bg-white" onClick={() => this.handleSubmit()}>Reserve</button>
                 </div>
-                <RenderButton min={0} max={8} {...pass} handleTimeSelect={this.handleTimeSelect}></RenderButton>
-                <RenderButton min={8} max={16} {...pass} handleTimeSelect={this.handleTimeSelect}></RenderButton>
-                <RenderButton min={16} max={24} {...pass} handleTimeSelect={this.handleTimeSelect}></RenderButton>
             </div>
         )
     }
