ebb5b12711e28d09f4363290c9a6561bd1a57470
Lasse Siniluoto <c6sila00@students.oamk.fi>
2020-12-09 11:49:34 +0200

    checkout add_robot

--- backend/routes/robots.js
+++ backend/routes/robots.js
@@ -31,6 +31,26 @@ router.get('/:name', (req, res) => {
         })
   });
 
+// get robots by type
+// GET http://localhost:3000/robots/type/2
+router.get('/:type', (req, res) => {
+  robots.query()
+    .findByType(req.params.type)
+    .then(robots => {
+      res.json(robots)
+    })
+});
+
+// get robots by payload
+// GET http://localhost:3000/robots/payload/2
+router.get('/:payload', (req, res) => {
+  robots.query()
+    .findByPayload(req.params.payload)
+    .then(robots => {
+      res.json(robots)
+    })
+});
+
 // delete robots by id
 // GET http://localhost:3000/robots/1
 router.get('/delete/:id', (req, res) => {
