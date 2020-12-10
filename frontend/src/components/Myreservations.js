6a16a2dfc29108d5869946af2ffb0253b84f6785
Elena <k0dael00@students.oamk.fi>
2020-12-10 21:11:16 +0100

    user managment filter, some navigation links, additional validation

--- frontend/src/components/Myreservations.js
+++ frontend/src/components/Myreservations.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom'
 const Table = () => {
 
    const [deleledId, setDeletedId] = useState(0);
+   const data = MyreservationsInfo;
 
    function renderTableHeader() {
       return <tr className="rHeader">
@@ -19,16 +20,16 @@ const Table = () => {
    }
 
    function deleteReservation(id) {
-      const deleteRobot = MyreservationsInfo.findIndex(function (i) {
+      const deleteRobot = data.findIndex(function (i) {
          return i.id === id;
       });
-      MyreservationsInfo.splice(deleteRobot, 1);
+      data.splice(deleteRobot, 1);
       setDeletedId(id);
       console.log(`Robot with id ${deleledId} has been successfully deleted`)
    }
 
    function renderTableData() {
-      return MyreservationsInfo.map((reservation, index) => {
+      return data.map((reservation, index) => {
          return (
             <tr key={reservation.id}>
                <td>{index + 1}</td>
@@ -52,7 +53,7 @@ const Table = () => {
          <Header />
          <div className="container2" style={{ height: "100vh" }}>
             <h1 id='title'>My reservations</h1>
-            <Link to="/User/Chooserobot"><button className="blueBtn" style={{ marginBottom: "5%" }}>Make a new reservation</button></Link>
+            <Link to="/user/robots"><button className="blueBtn" style={{ marginBottom: "5%" }}>Make a new reservation</button></Link>
             <table id='reservations'>
                <tbody>
                   {renderTableHeader()}
