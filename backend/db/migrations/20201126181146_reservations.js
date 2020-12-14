e293b25f57a58afbe42dc1d423b7ee4fc80f59aa
unknown <t7soee00@students.oamk.fi>
2020-12-14 14:13:41 +0200

    more API fixes

--- backend/db/migrations/20201126181146_reservations.js
+++ backend/db/migrations/20201126181146_reservations.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('reservations', (table) => {
-        table.increments('id').unsigned().primary();
+        table.increments('reservartionId').unsigned().primary();
         table.integer('userId').unsigned();
         table.foreign('userId').references('id').inTable('users');
         table.integer('robotId').unsigned();
