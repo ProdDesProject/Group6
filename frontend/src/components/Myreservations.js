e832c94bf88f3cee8c8f813537f44b3f4f31dd56
Long <longlong2000hcm@gmail.com>
2020-12-14 20:43:21 +0200

    function convertime to UI friendly

--- frontend/src/components/Myreservations.js
+++ frontend/src/components/Myreservations.js
@@ -3,7 +3,7 @@ import React, { Component, useState } from "react";
 import { Link } from 'react-router-dom'
 import Axios from "axios";
 import domain from "../domain"
-
+import LoadingScreen from "./LoadingScreen";
 
 
 function renderTableHeader() {
@@ -17,9 +17,6 @@ function renderTableHeader() {
 }
 
 
-
-
-
 function renderTableData({data, DeleteReservation}) {
    return data.map((reservation, index) => {
       return (
@@ -27,9 +24,9 @@ function renderTableData({data, DeleteReservation}) {
             <td>{reservation.id}</td>
             <td>{reservation.robotname}</td>
             <td>{convertDate(reservation.date)}</td>
-            <td>{reservation.time}</td>
+            <td>{convertTime(reservation.time)}</td>
             <td><button className="deleteRes" onClick={() => {
-               var message = `Are you sure you want to delete this reservation?\n\nRobot's name: ${reservation.robotname} \nDate: ${reservation.date}\nTime: ${reservation.time}`
+               var message = `Are you sure you want to delete this reservation?\n\nReservation #: ${reservation.id}\nRobot's name: ${reservation.robotname} \nDate: ${reservation.date}`
                if (window.confirm(message))
                   DeleteReservation(reservation.id)
             }}>
@@ -44,6 +41,30 @@ function renderTableData({data, DeleteReservation}) {
 const convertDate = (str) => {
    let normalDate = str.substring(8, 10) + "/" + str.substring(5, 7) + "/" + str.substring(0, 4);
    return normalDate;
+}
+
+
+// convert string of array to timve divs
+const convertTime = (str) => {
+   let timeArray = JSON.parse(str);
+   timeArray.sort((a,b)=>{return a-b})
+   console.log(timeArray)
+   let temp = []
+   let start = null;
+   let end = null;
+   for (let i=0; i<timeArray.length; i++ ) {
+      if (start===null) {
+         start = timeArray[i]
+      }
+      if (start!==null && timeArray[i]+1!==timeArray[i+1]) {
+         end = timeArray[i]
+         temp.push({start, end})
+         start = null
+         end = null
+      }
+   }
+   let result = temp.map(i=><><div>{i.start}:00 - {i.end+1}:00</div></>)
+   return result
 }
 
 export default class Myreservations extends Component {
@@ -98,6 +119,7 @@ export default class Myreservations extends Component {
    render() {
       return (
          <div>
+            {this.state.loading?<LoadingScreen/>:null}
             <div className="container2" style={{ height: "100vh" }}>
                <h1 id='title'>My reservations</h1>
                <Link to="/user/robots"><button className="blueBtn" style={{ marginBottom: "5%" }}>Make a new reservation</button></Link>
